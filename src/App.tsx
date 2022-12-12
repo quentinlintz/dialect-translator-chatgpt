9025191ce783bf97bcbaecd331e1451d3536cf37
Quentin Lintz <quentinlintz@outlook.com>
2022-12-12 15:48:49 -0500

    Iteration 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from 'react';
+import {
+  Container,
+  Title,
+  Button,
+  Input,
+  Textarea,
+} from '@mantine/core';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [csvFile, setCsvFile] = useState(null);
+  const [sentence, setSentence] = useState('');
+  const [translatedText, setTranslatedText] = useState('');
+
+  const handleFileSelect = (event) => {
+    setCsvFile(event.target.files[0]);
+  }
+
+  const handleTranslateClick = () => {
+    // Implement translation logic here using the csvFile and sentence
+    // Then set the translated text using setTranslatedText
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <Container>
+      <Title>Dialect Translator</Title>
+      <input type="file" accept=".csv" onChange={handleFileSelect} />
+      <Input value={sentence} onChange={(event) => setSentence(event.target.value)} />
+      <Button onClick={handleTranslateClick}>Translate</Button>
+      <Textarea value={translatedText} />
+    </Container>
+  );
+};
 
 export default App
